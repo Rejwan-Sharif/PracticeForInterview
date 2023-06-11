a7e4264d4d2d22256a9f2c5dbb65241d537ca99d
Rejwan-Sharif <mohammedrejwan8@gmail.com>
2023-06-12 01:07:50 +0600

    swap number with and without using third variable

--- src/ProblemSolving/Swap2numbers.java
+++ src/ProblemSolving/Swap2numbers.java
@@ -14,21 +14,41 @@ public class Swap2numbers {
     public static void main(String[] args) {
         Scanner input =  new Scanner (System.in);
         
-         System.out.print("Enter a value for a : ");
-        int a = input.nextInt();
-         System.out.print("Enter a value for b : ");
-        int b = input.nextInt();
-       
-        int c=0;
+//         System.out.print("Enter a value for a : ");
+//        int a = input.nextInt();
+//         System.out.print("Enter a value for b : ");
+//        int b = input.nextInt();
         
-        c = b;
-        b = a;
-        a = c;
+//    with using  third variable
+
+//        int c=0;
+//        
+//        c = b;
+//        b = a;
+//        a = c;
+//        System.out.println("After swaping the value of a : "+a);
+//        System.out.println("After swaping the value of b : "+b);
+        
+//     without using third variable
+
+         System.out.print("Enter a value for x : ");
+        int x = input.nextInt();
+         System.out.print("Enter a value for y : ");
+        int y = input.nextInt();
+        
+//        x = x+y;
+//        y = y+x;
+//        x = y-x;
+//        y = y-x-x;
+        
+        x = x+y;
+        y = x-y;
+        x = x-y;
         
         
-        System.out.println("After swaping the value of a : "+a);
-        System.out.println("After swaping the value of b : "+b);
         
+        System.out.println("After swaping the value of x :"+x);
+        System.out.println("After swaping the value of y :"+y);
         
     }
     
